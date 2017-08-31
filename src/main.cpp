13fc03cc50805df897d51c4bb1530abd5827f7b7
Daniel "Alex" Daley-Montgomery <ddaley@seas.upenn.edu>
2017-08-31 00:15:32 -0700

    Added name to main.cpp and a screenshot

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,8 @@
  * C main function.
  */
 int main(int argc, char* argv[]) {
-    // TODO: Change this line to use your name!
-    m_yourName = "TODO: YOUR NAME HERE";
+
+    m_yourName = "Alex Daley-Montgomery";
 
     if (init(argc, argv)) {
         mainLoop();
